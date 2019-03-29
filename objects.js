48c4397a7587c3a6b70b73e213d0116e8855bcff
Olu Isaac <oluisaac7@gmail.com>
2019-03-29 21:45:17 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -14,7 +14,11 @@ playlist[artistName] = songTitle;
 return playlist;
 }
 
-function removeFromPlaylist(playlist, artistName){
-delete playlist.[artistName];
-return playlist;
+function removeFromPlaylist(Playlist, artistName) {
+  delete Playlist[artistName];
+  return Playlist;
 }
+
+removeFromPlaylist(playlist, "artistName");
+
+};
