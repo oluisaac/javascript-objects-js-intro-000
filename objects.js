92e13d3cc82db851b3b01577ba4c38974e2b0b0f
Olu Isaac <oluisaac7@gmail.com>
2019-03-29 21:07:19 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -10,7 +10,7 @@ var playlist = {
 };
 
 function updatePlaylist(Playlist, artistName, songTitle) {
-  return object.assign({}, Playlist, { [artistName]: songTitle});
+  return object.assign({}, Playlist, {[artistName]: songTitle});
 
  function removeFromPlaylist(Playlist, artistName) {
    delete Playlist[artistName];
