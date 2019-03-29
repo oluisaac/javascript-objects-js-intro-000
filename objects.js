90b7c9bb9f62e040e43cc87528045402956d8710
Olu Isaac <oluisaac7@gmail.com>
2019-03-29 20:31:09 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,7 +1,13 @@
 
-var playlist = {artistName: "Michael", songTitle: "Slowdive"};
-var playlist = {artistName: "Mariah", songTitle: "My Bloody Valentine"};
-var playlist = {artistName: "Beyonce", songTitle: "Phil Ochs"};
+var playlist = {
+  artistName: "Michael", songTitle: "Slowdive"
+};
+var playlist = {
+  artistName: "Mariah", songTitle: "My Bloody Valentine"
+};
+var playlist = {
+  artistName: "Beyonce", songTitle: "Phil Ochs"
+};
 
 function updatePlaylist(Playlist, artistName, songTitle) {
   return object.assign({}, artistName, { [playlist]: songTitle});
